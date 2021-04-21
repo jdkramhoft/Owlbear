9c34f8f00db135abbed412bdc8dc1620b9519464
jdkramhoft <s175445@student.dtu.dk>
2021-04-21 11:57:11 +0200

    Initial Twitter bot project added Testing link feature

---
+++ Twitterjack/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Twitterjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
